d987254e4e348c36e4736c5b651d57145f6e3185
Chris Rytting <christophermichaelrytting@gmail.com>
2019-07-23 10:14:42 -0600

    replicated sutton and barto results

--- bandit_epsilon_test.py
+++ bandit_epsilon_test.py
@@ -1,50 +1,73 @@
-from scipy.stats import multinomial as mn
 import numpy as np
+import seaborn
+from matplotlib import pyplot as plt
 
 class BanditN:
-    def __init__(self,n):
-        self.n = n
-        self.rewards = np.random.multivariate_normal(np.zeros(n), np.eye(n))
-        print(np.shape(self.rewards))
+    def __init__(self,action_values):
+        self.action_values = action_values
+        self.average_rewards = np.zeros(len(action_values))
+        self.n_pulls = np.zeros(len(action_values))
+
+    def play(self,eps):
+        #Give reward of q(A_t) + \eps where \eps~N(0,1)
+        index = np.argmax(self.average_rewards)
+        other_indices = np.delete(np.arange(len(self.average_rewards)), index)
+        final_index = np.random.choice([index, np.random.choice(other_indices)], 
+                p = [1-eps, eps])
+
+        self.n_pulls[final_index] += 1
+        reward = self.action_values[final_index] + np.random.normal()
+        self.update_average_rewards(reward, final_index)
+        #print(
+        #"""
+        #   On iteration: {}
+        #  Action Values: {}
+        #Average rewards: {}
+        #        N_pulls: {}
+        #            Eps: {}
+        #     You played: {}
+        #         Reward: {}
+        #""".format(j,
+        #    self.action_values,
+        #    self.average_rewards,
+        #    self.n_pulls,
+        #    eps, 
+        #    final_index, 
+        #    reward)
+        #)
 
         
+        return reward
 
-        #self.array = [1,0]
-        #self.probs = [[.1,.9], [.5,.5], [.9,.1]]
-        #self.n = np.zeros(3)
-        #self.reward = rewards
-        #self.reward = np.random.multivariate_normal(mean
-
-    def play(self,arm):
-        reward = self.rewards[arm-1] + np.random.normal()
-
-        #i = 0
-        #for i in range(10000):
-        #    i+=1
-        #    #print('Round {}'.format(i))
-        #    answer = np.random.choice(np.arange(1,4))
-        ##    answer = input('Which arm do you want to pull?')
-        #    answer = int(answer)
-        #    #print('Your answer: %d' % answer)
-        #    #print(self.probs[answer - 1])
-        #    reward = np.random.choice([0,1],1,p=self.probs[answer - 1])
-        #    #print('Reward = {}!'.format(reward))
-        #    self.total_reward[answer - 1] += reward
-        #    self.n[answer-1] += 1
-        #print('N pulls on each arm: ', self.n)
-        #print('Average Reward = {}'.format(self.total_reward/self.n))
-        #print('Total Reward = {}'.format(self.total_reward))
-
-
+    def update_average_rewards(self,reward, index):
+        n_pulls = self.n_pulls[index]
+        self.average_rewards[index] = (reward + self.average_rewards[index] * (n_pulls-1)) / n_pulls
 
 
 
 if __name__=="__main__":
+    #Different tolerances
+    n = 10
+    action_values = np.random.multivariate_normal(np.zeros(n), np.eye(n))
+    bandit = BanditN(action_values)
+
     for eps in [0,0.1,0.01]:
+        #Different experiments
+        reward_trajectory_list = []
         for i in range(2000):
-            bandit = BanditN(n)
-            for j in range(1000):
+            print('{}/2000'.format(i))
+            action_values = np.random.multivariate_normal(np.zeros(n), np.eye(n))
+            bandit = BanditN(action_values)
+            reward_trajectory = []
+            for j in range(100):
+                reward = bandit.play(eps)
+                reward_trajectory.append(reward)
+            reward_trajectory_list.append(reward_trajectory)
+        #average all runs
+        averages = np.array(reward_trajectory_list).mean(axis = 0)
+        plt.plot(averages, label = 'eps = {}'.format(eps))
+        plt.legend()
+    plt.show()
 
 
-    for i in range(1000):
-        bandit.play
+
